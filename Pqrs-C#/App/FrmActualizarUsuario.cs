d1cd3486b520edd971e55ef60ed710cccdc780e2
migueljgc <miguelgaviriam.8@gmail.com>
2024-04-16 11:55:00 -0500

    actilizar

---
+++ Pqrs-C#/App/FrmActualizarUsuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pqrs_C_.App
+{
+    public partial class FrmActualizarUsuario : Form
+    {
+        public FrmActualizarUsuario()
+        {
+            InitializeComponent();
+        }
+    }
+}
